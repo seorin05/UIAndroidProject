46a2613d0bb9a58a9f04c3d5f9c2569c355d60de
서린 <madeleine05@soongsil.ac.kr>
2025-11-20 16:27:07 +0900

    화면 전환 기능 추가

--- app/src/main/java/com/example/myapplication/G_sche_main.java
+++ app/src/main/java/com/example/myapplication/G_sche_main.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,14 @@ public class G_sche_main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_gsche_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnAddSchedule = findViewById(R.id.btnAddSchedule);
+        btnAddSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(G_sche_main.this, G_sche_add.class);
+                startActivity(intent);
+            }
         });
     }
 }
