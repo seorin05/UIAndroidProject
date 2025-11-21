4f69d50e4fe31065d332620a6aa0e865af6b03ce
heejin <heeijn05@gmail.com>
2025-11-21 17:45:36 +0900

    어르신 문답 화면 이동 구현

--- app/src/main/java/com/example/myapplication/senior/Qna_main.java
+++ app/src/main/java/com/example/myapplication/senior/Qna_main.java
@@ -9,18 +9,36 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.myapplication.R;
+import com.example.myapplication.databinding.ActivityQnaMainBinding;
+
+import android.content.Intent;
 
 public class Qna_main extends AppCompatActivity {
+
+    private ActivityQnaMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_qna_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        binding = ActivityQnaMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        binding.tell.setOnClickListener(v -> {
+            Intent intent = new Intent(Qna_main.this, Qna_tell.class);
+            startActivity(intent);
+        });
+
+        binding.listen.setOnClickListener(v -> {
+            Intent intent = new Intent(Qna_main.this, Qna_listen.class);
+            startActivity(intent);
         });
     }
 }
