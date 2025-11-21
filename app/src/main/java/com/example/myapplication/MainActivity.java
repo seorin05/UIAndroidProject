d76772af25722d84ed15dab3334fc927070e1858
gPdusrla <168806106+gPdusrla@users.noreply.github.com>
2025-11-21 11:41:34 +0900

    메인액티비티 살림

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,39 +1,24 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        // 버튼 찾기
-        Button gotoSeniorButton = findViewById(R.id.btn_senior);
-        Button gotoGuardianButton = findViewById(R.id.btn_guardian);
-
-        // 클릭 리스너 설정
-        gotoSeniorButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // SubActivity.class 대신 SeniorMain.class로 변경
-                Intent intent = new Intent(getApplicationContext(), SeniorMain.class);
-                startActivity(intent);
-            }
-        });
-        gotoGuardianButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // SubActivity.class 대신 SeniorMain.class로 변경
-                Intent intent = new Intent(getApplicationContext(), GuardianTodoMain.class);
-                startActivity(intent);
-            }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
     }
 }
