bc71c4acef7cc01f2841595f06344b06e1623f9a
gPdusrla <168806106+gPdusrla@users.noreply.github.com>
2025-11-21 11:59:46 +0900

    폴더 정리2

--- app/src/main/java/com/example/myapplication/guardian/G_sche_main.java
+++ app/src/main/java/com/example/myapplication/guardian/G_sche_main.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.myapplication.guardian;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,9 +7,8 @@ import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.R;
 
 public class G_sche_main extends AppCompatActivity {
 
