21e0b27c8e60db999cb0da0501aa7e4b7349efb2
gPdusrla <168806106+gPdusrla@users.noreply.github.com>
2025-11-25 11:26:39 +0900

    어르신 메인회면 화면이동(전화 앱으로 이동)

--- app/src/main/java/com/example/myapplication/senior/SeniorMain.java
+++ app/src/main/java/com/example/myapplication/senior/SeniorMain.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.net.Uri; // 전화
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +19,9 @@ public class SeniorMain extends AppCompatActivity {
 
         // 버튼 찾기
         Button todoButton = findViewById(R.id.btn_todo);
+        Button scheButton = findViewById(R.id.btn_schedule);
+        Button qnaButton = findViewById(R.id.btn_qa);
+        Button callButton = findViewById(R.id.btn_call);
 
         // 클릭 리스너 설정
         todoButton.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +29,29 @@ public class SeniorMain extends AppCompatActivity {
             public void onClick(View view) {
                 // SubActivity.class 대신 SeniorMain.class로 변경
                 Intent intent = new Intent(getApplicationContext(), SeniorTodo.class);
+                startActivity(intent);
+            }
+        });
+        scheButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // SubActivity.class 대신 SeniorMain.class로 변경
+                Intent intent = new Intent(getApplicationContext(), ScheduleMainActivity.class);
+                startActivity(intent);
+            }
+        });
+        qnaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // SubActivity.class 대신 SeniorMain.class로 변경
+                Intent intent = new Intent(getApplicationContext(), Qna_main.class);
+                startActivity(intent);
+            }
+        });
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intent.ACTION_DIAL);
                 startActivity(intent);
             }
         });
